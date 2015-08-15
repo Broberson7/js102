c93496d3cab55b7021fe52b8ab4a67c3849cf2b4
Sybia Roberson <sybia.roberson@gmail.com>
2015-08-15 14:00:07 -0500

    Testing

---
+++ main.js
@@ -0,0 +1,16 @@
+var numberList = [4, 20, 44, 17, 3, 19, 23, 25, 60];
+
+numberList.sort(function(a, b){return a-b});
+
+
+
+console.log(numberList);
+
+function testingNumbers (numberList) {
+	
+}
+
+
+// Write a JavaScript function that accepts an array, and returns 
+// a new array that contains 
+// elements in the array you passed in that are larger than 25.
